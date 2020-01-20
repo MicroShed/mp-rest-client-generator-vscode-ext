19813f96f493cebcedb6977a48a7de753be54aca
Benson Ning <bensonn@ca.ibm.com>
2020-01-20 13:37:39 -0500

    Extension supports new OpenAPI generator
    Signed-off-by: Benson Ning <bensonn@ca.ibm.com>

--- src/util/GenerateProject.ts
+++ src/util/GenerateProject.ts
@@ -43,13 +43,15 @@ export async function generateProject(fileUri): Promise<void> {
         }
     }
 
+    const path = require('path');
+
     // prompt user to select which folder to generate into, if started from a file default ot the parent folder of that file
     if (fileSelectedUri) {
         this.assumeChoice = await util.specifyTargetFolder(fileSelectedUri);
         if (this.assumeChoice !== undefined) {
             this.srcDir = vscode.Uri.parse(this.assumeChoice).fsPath;
             console.log("Selected srcDir: " + this.srcDir);
-            this.package = util.getPackagePath(this.srcDir);
+            this.package = await util.getPackageName(this.srcDir, path.sep);
         } else {
             console.error("Target folder not specified.");
         }
@@ -58,7 +60,7 @@ export async function generateProject(fileUri): Promise<void> {
         if (this.assumeChoice !== undefined) {
             this.srcDir = vscode.Uri.parse(this.assumeChoice).fsPath;
             console.log("Selected srcDir: " + this.srcDir);
-            this.package = util.getPackagePath(this.srcDir);
+            this.package = await util.getPackageName(this.srcDir, path.sep);
         } else {
             console.error("Target folder not specified.");
         }
@@ -66,75 +68,92 @@ export async function generateProject(fileUri): Promise<void> {
 
     if (this.package !== undefined && this.yamlFile !== undefined && this.srcDir !== undefined && this.assumeChoice !== undefined) {
         // Call MicroProfile generator
-        this.packagePath = this.package;
-        this.package = this.package.replace(/\//g, ".");
+        this.packagePath = this.package.replace(".", path.sep);
         var commands = [];
         var jarPath = __dirname + '/../openapi-generator-cli.jar';
 
         // execute the MicroProfile generator jar
         var jarCommand = 'java -jar ' + jarPath + ' generate '
             + '-p useMultipart=false \ '
-            + '-i ' + this.yamlFile + ' -g microprofile-rest-client -o ' + this.srcDir + '\
+            + '-i ' + this.yamlFile + ' -g java --library microprofile -o ' + this.srcDir + '\
             --api-package ' + this.package + '.api \
             --model-package ' + this.package + '.models';
         commands.push(jarCommand);
     }
-    var exec = require('child_process').exec;
-    function runCommands(array, srcDir, packagePath, callback) {
-        var index = 0;
-        var results = [];
-        function next() {
-            if (index < array.length) {
-                exec(array[index++], function (err, stdout) {
-                    if (err) { return callback(err); }
-                    // do the next iteration
-                    results.push(stdout);
-                    next();
-                });
-            } else {
-                callback(null, results, srcDir, packagePath);
+
+    if (commands && commands.length > 0) {  // there are commands to run
+        var exec = require('child_process').exec;
+        function runCommands(array, srcDir, packagePath, callback) {
+            var index = 0;
+            var results = [];
+            function next() {
+                if (index < array.length) {
+                    exec(array[index++], function (err, stdout) {
+                        if (err) { return callback(err); }
+                        // do the next iteration
+                        results.push(stdout);
+                        next();
+                    });
+                } else {
+                    callback(null, results, srcDir, packagePath);
+                }
             }
+            // start the first iteration
+            next();
         }
-        // start the first iteration
-        next();
+
+        vscode.window.withProgress( // run the command with a "progress" dialog
+			{
+				location: vscode.ProgressLocation.Notification,
+				title: "Generating the MicroProfile Rest Client interface template..."
+			}, async () => {
+                return new Promise((resolve) => {
+                    runCommands(commands, this.srcDir, this.packagePath, function (err, results, srcDir, packagePath) {
+                        if (!err) {
+                            try {
+                                let generatedPath = path.resolve(srcDir, 'src', 'main', 'java', packagePath);
+            
+                                // move models and apis folders to srcDir
+                                var ncp = require('ncp').ncp;
+                                ncp(generatedPath, srcDir, function (err) {
+                                    if (err) {
+                                        return console.error(err);
+                                    }
+                                    console.log('Moved ' + generatedPath + ' to ' + srcDir);
+                                });
+            
+                                // remove src dir created by generator
+                                util.deleteDirectory(path.resolve(srcDir, 'src'));
+            
+                                // remove .openapi-generator dir created by generator
+                                util.deleteDirectory(path.resolve(srcDir, '.openapi-generator'));
+            
+                                // remove pom.xml created by generator
+                                util.deleteFile(path.resolve(srcDir, 'pom.xml'));
+            
+                                // remove .openapi-generator created by generator
+                                util.deleteFile(path.resolve(srcDir, '.openapi-generator-ignore'));
+            
+                                // remove docs folder created by generator
+                                util.deleteDirectory(path.resolve(srcDir, 'docs'));
+            
+                                // remove README.md file created by generator
+                                util.deleteFile(path.resolve(srcDir, 'README.md'));
+                            } catch (error) {
+                                console.error(error);
+                            }
+                        }
+                        resolve();
+                        if (err) {
+                            console.error(err);
+                            vscode.window.showErrorMessage("An error has occurred. Unable to generate a rest client in the specified directory.  Check the debug console for more information.");
+                        } else {
+                            vscode.window.showInformationMessage("Successfully generated a MicroProfile Rest Client interface template.");
+                        }
+                    });
+                });
+			}
+		);
     }
-
-    runCommands(commands, this.srcDir, this.packagePath, function (err, results, srcDir, packagePath) {
-        if (err) {
-            console.error(err);
-            vscode.window.showErrorMessage("An error has occurred. Unable to generate a rest client in the specified directory.  Check the debug console for more information.");
-        }
-        if (results) {
-            vscode.window.showInformationMessage("Successfully generated a MicroProfile Rest Client interface template.");
-            try {
-                const path = require('path');
-                let generatedPath = path.resolve(srcDir, 'src', 'gen', 'java', packagePath);
-
-                // move models and apis folders to srcDir
-                var ncp = require('ncp').ncp;
-                ncp(generatedPath, srcDir, function (err) {
-                    if (err) {
-                        return console.error(err);
-                    }
-                    console.log('Moved ' + generatedPath + ' to ' + srcDir);
-                });
-
-                // remove src dir created by generator
-                util.deleteDirectory(path.resolve(srcDir, 'src'));
-
-                // remove .openapi-generator dir created by generator
-                util.deleteDirectory(path.resolve(srcDir, '.openapi-generator'));
-
-                // remove pom.xml created by generator
-                util.deleteFile(path.resolve(srcDir, 'pom.xml'));
-
-                // remove .openapi-generator created by generator
-                util.deleteFile(path.resolve(srcDir, '.openapi-generator-ignore'));
-
-            } catch (err) {
-                console.error(err);
-            }
-        }
-    });
 }
 
