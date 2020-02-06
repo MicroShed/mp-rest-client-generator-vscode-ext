f5060c23ab3f0411e37310d330894c39e8db5088
Ryan Zegray <ryan.zegray@gmail.com>
2020-02-06 10:08:30 -0500

    eslint
    Signed-off-by: Ryan Zegray <ryan.zegray@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const GENERATE_FROM_YAML_FILE = "Generate from a .yaml file";
-export const GENERATE_FROM_YAML_URL =  "Generate from a url";
+export const GENERATE_FROM_YAML_URL = "Generate from a url";
--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,14 @@
-import * as vscode from 'vscode';
-import { generateProject } from './util/GenerateProject';
+import * as vscode from "vscode";
+import { generateProject } from "./util/GenerateProject";
 
-export async function activate(context: vscode.ExtensionContext): Promise<void> {
-	console.log('extension "vscode-microprofile-generator" is now active!');
-	context.subscriptions.push(vscode.commands.registerCommand("microprofile.restclient.generate", (fileUri) => {
-		generateProject(fileUri);
-	}));
-
+export function activate(context: vscode.ExtensionContext): void {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("microprofile.restclient.generate", fileUri => {
+      generateProject(fileUri);
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export function deactivate(): void {}
